c246dc61a66173b58efe5941a0f4497d338592c0
Sky Shabatura <sky@sky.camp>
2017-12-30 14:27:01 -0600

    Refactored Rootstrap Folder structure.

--- app/includes/customizer/class-taproot-customizer.php
+++ app/includes/customizer/class-taproot-customizer.php
@@ -87,7 +87,13 @@ if( !class_exists( 'Taproot_Customizer' ) )
                 /**
                  * The file that initiates rootstrap object
                  */                
-                require_once TAPROOT_LIBRARY . '/wp-rootstrap/wp-rootstrap.php';
+                require_once TAPROOT_LIBRARY . '/wp-rootstrap/src/wp-rootstrap.php';
+
+                // load the default Rootstrap settings from the theme. 
+                $rootstrap_defaults = include( TAPROOT_CUSTOMIZER . '/inc/taproot-rootstrap-settings.php' );
+
+                // Load the Rootstrap config
+                $rootstrap->load_config( $rootstrap_defaults );                
             }
             else
             {
@@ -95,13 +101,7 @@ if( !class_exists( 'Taproot_Customizer' ) )
             }
 
             // Set the Roostrap uri
-            $rootstrap->set_uri( TAPROOT_LIBRARY_URI . '/wp-rootstrap/' );
-
-            // load the default Rootstrap settings from the theme. 
-            $rootstrap_defaults = include( TAPROOT_CUSTOMIZER . '/inc/taproot-rootstrap-settings.php' );
-
-            // Load the Rootstrap config
-            $rootstrap->load_config( $rootstrap_defaults );
+            $rootstrap->set_uri( TAPROOT_LIBRARY_URI . '/wp-rootstrap/src/' );
 
             // Store the Rootstrap object
             $this->rootstrap = $rootstrap;     
