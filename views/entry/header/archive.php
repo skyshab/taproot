c0ced53d19333a1d2e26844952975a9afffa37b2
Sky Shabatura <sky@sky.camp>
2020-06-14 10:04:30 -0500

    Entry changes

--- views/entry/header/archive.php
+++ views/entry/header/archive.php
@@ -1,8 +1,6 @@
 <div class="archive-header <?php printf( 'archive-header--%s', esc_attr( get_post_type() ) ) ?>" >
 
-    <h1 class="archive-header__title <?php printf( 'archive-header__title--%s', esc_attr( get_post_type() ) ) ?>">
-        <?php the_archive_title() ?>
-    </h1>
+    <?php Hybrid\app('post-types/template')->the_title( ['class' => sprintf( 'archive-header__title archive-header__title--%s', get_post_type() )] ) ?>
 
     <?php if ( ! is_paged() && get_the_archive_description() ) : ?>
 
