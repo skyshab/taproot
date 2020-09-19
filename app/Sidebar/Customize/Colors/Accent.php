f3c33a0f0b8d272afd32d4d6b17aba8abed0d875
Sky Shabatura <sky@sky.camp>
2020-09-19 11:01:59 -0500

    Sidebar mods

---
+++ app/Sidebar/Customize/Colors/Accent.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Theme Color.
+ *
+ * This class handles the customizer control for a theme color.
+ *
+ * @package   Taproot
+ * @author    Sky Shabatura
+ * @copyright Copyright (c) 2020, Sky Shabatura
+ * @link      https://github.com/skyshab/taproot
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+namespace Taproot\Sidebar\Customize\Colors;
+
+use Taproot\Customize\Controls\Color\Color;
+use Taproot\Customize\Traits\CustomPropertyStyles;
+use Taproot\Customize\Traits\CustomPropertyPreview;
+
+/**
+ * Class for color controls
+ *
+ * @since  2.0.0
+ * @access public
+ */
+class Accent extends Color {
+
+    use CustomPropertyStyles;
+    use CustomPropertyPreview;
+
+    /**
+     * Control name
+     *
+     * @since 2.0.0
+     * @var string
+     */
+    public $id = 'sidebar--accent-color';
+
+    /**
+     * Control label
+     *
+     * @since 2.0.0
+     * @var string
+     */
+    public $label = 'Accent Color';
+
+    /**
+     * Hide the alpha channel
+     *
+     * @since 2.0.0
+     * @var bool
+     */
+    public $hide_alpha = TRUE;
+}
